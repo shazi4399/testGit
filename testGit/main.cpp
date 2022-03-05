67849a53e3d85f9908042efe655ef066992a9944
zhanghao <2573344091@qq.com>
2022-03-05 21:09:53 +0800

    【01两数之和】题解。

--- testGit/main.cpp
+++ testGit/main.cpp
@@ -1,14 +1,41 @@
-//
-//  main.cpp
-//  testGit
-//
-//  Created by zhanghao on 2022/3/5.
-//
+/// Source : https://leetcode.com/problems/two-sum/description/
+/// Author : liuyubobobo
+/// Time   : 2017-11-15
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+
+/// Brute Force
+/// Time Complexity: O(n^2)
+/// Space Complexity: O(1)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        for(int i = 0 ; i < nums.size() ; i ++)
+            for(int j = i + 1 ; j < nums.size() ; j ++)
+                if(nums[i] + nums[j] == target)
+                    return {i, j};
+
+        throw invalid_argument("the input has no solution");
+    }
+};
+
+
+void print_vec(const vector<int>& vec){
+    for(int e: vec)
+        cout << e << " ";
+    cout << endl;
+}
+
+int main() {
+
+    vector<int> nums = {0,4,3,5};
+    int target = 10;
+    print_vec(Solution().twoSum(nums, target));
+
     return 0;
 }
